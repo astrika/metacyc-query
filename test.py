dd5418ea41a0891ebd687de90a87e710c4834fa4
astrika <astrika.0914@gmail.com>
2017-04-14 12:45:07 -0400

    Add more queries

--- test.py
+++ test.py
@@ -2,4 +2,13 @@ import pythoncyc.config as config
 config.set_host_name('castalia.cs.fiu.edu')
 import pythoncyc
 meta = pythoncyc.select_organism('meta')
+print pythoncyc.all_orgids()
+meta.trp
 print meta
+rxn9000 = meta.rxn9000
+print meta.trp.frameid
+rxns_trp_right = [meta[fid] for fid in meta.trp.appears_in_right_side_of]
+reactions = meta.reactions
+print reactions
+organisms = meta.organisms
+print organisms
