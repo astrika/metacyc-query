b8beddfec602f48c92d5adc5312e18106d9bcb3e
astrika <astrika.0914@gmail.com>
2017-04-21 14:28:30 -0400

    added comments

--- test.py
+++ test.py
@@ -5,18 +5,28 @@ import pythoncyc
 #this creates PGDB object associated with meta(MetaCyc)
 meta = pythoncyc.select_organism('meta')
 print pythoncyc.all_orgids()
+#attributing trp data frame to meta
 meta.trp
+"""
+prints: <PGDB meta, currently has 1 PFrames>, which shows that trp data frame
+was attrubuted to meta
+"""
 print meta
 rxn9000 = meta.rxn9000
 print meta.trp.frameid
+#a list of PFrames of reactions that contain TRP on right side
 rxns_trp_right = [meta[fid] for fid in meta.trp.appears_in_right_side_of]
 print rxns_trp_right
 reactions = meta.reactions
 print reactions
 organisms = meta.organisms
 print organisms
+#accesses a certain instance in PFrame reactions
 print reactions.instances[0]
+#what is on the left side of this reactions instance
 print reactions[0].left
+#what is on the right side of this reactions instance
 print reactions[0].right
+#method accesses an instance without the use of Pframes but rather just frame ids
 print meta.get_slot_values('TRP', 'CHEMICAL-FORMULA')
 print meta
--- tutorialexample1.py
+++ tutorialexample1.py
@@ -1,5 +1,5 @@
 """
-The following is retrieved from PythonCyc tutorial:
+The following is an example obtained from PythonCyc tutorial:
 https://github.com/latendre/PythonCyc/blob/master/doc/tutorial.md#pframes
 """
 
@@ -11,9 +11,12 @@ def create_pgdb_with_compounds_and_reactions(orgid):
      Create a PythonCyc PGDB object with all its compounds and reactions
      PFrames created. Return the PGDB object.
      """
+     #creates pgdb associated to given orgid
      pgdb = pythoncyc.select_organism(orgid)
+     #attributes compounds and reactions data frames to pgdb
      pgdb.compounds
      pgdb.reactions
      return pgdb
+#test for the example above
 pgdb = create_pgdb_with_compounds_and_reactions('ecoli')
 print pgdb
