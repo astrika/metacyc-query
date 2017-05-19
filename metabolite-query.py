410cf7d22ba5b5fb1063f897b05e34efe969b107
astrika <astrika.0914@gmail.com>
2017-05-18 22:16:29 -0400

    deleted unecessary line

--- metabolite-query.py
+++ metabolite-query.py
@@ -3,6 +3,4 @@ import pythoncyc.config as config
 config.set_host_name('castalia.cs.fiu.edu')
 import pythoncyc
 #this creates PGDB object associated with meta(MetaCyc)
-meta = pythoncyc.select_organism('meta')
-print meta.get_slot_values('TRP', 'CHEMICAL-FORMULA')
 print meta.pathways_of_compound('1,2-dilinoleoyl-sn-glycero-3-phosphocholine')
