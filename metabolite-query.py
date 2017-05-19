81eb8cea127519ac0dccb406bae0cbca64d7b5ce
astrika <astrika.0914@gmail.com>
2017-05-18 22:20:16 -0400

    added a comment

--- metabolite-query.py
+++ metabolite-query.py
@@ -4,4 +4,5 @@ config.set_host_name('castalia.cs.fiu.edu')
 import pythoncyc
 #this creates PGDB object associated with meta(MetaCyc)
 meta = pythoncyc.select_organism('meta')
+#prints pathways of compound specified
 print meta.pathways_of_compound('1,2-dilinoleoyl-sn-glycero-3-phosphocholine')
